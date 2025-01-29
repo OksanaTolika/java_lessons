c944f2e1e0331d2234399a38e0fa286d56585fcc
Oksana Tolika <tolika_oksana@mail.ru>
2025-01-29 23:55:16 +0300

    Homework 1

---
+++ src/lesson1/home/src/main/java/org/example/HomeWorkApp.java
@@ -0,0 +1,65 @@
+package org.example;
+
+public class HomeWorkApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("<---printThreeWords--->");
+        printThreeWords();
+        System.out.println();
+        System.out.println("<---checkSumSign--->");
+        checkSumSign();
+        System.out.println();
+        System.out.println("<---printColor--->");
+        printColor();
+        System.out.println();
+        System.out.println("<---compareNumbers--->");
+        compareNumbers();
+    }
+
+
+    public static void printThreeWords()
+    {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign()
+    {
+        int a = 5, b = -5;
+        int sum = a + b;
+
+        if (sum >= 0)
+        {
+            System.out.println("Сумма положительная");
+        }
+        else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor()
+    {
+        int value = 35;
+
+        if (value <= 0)
+        {
+            System.out.println("Красный");
+        }
+        else if (value > 0 && value <= 100)
+        {
+            System.out.println("Желтый");
+        }
+        else System.out.println("Зеленый");
+    }
+
+    public static void compareNumbers()
+    {
+        int a = 5, b = -5;
+
+        if (a >= b)
+        {
+            System.out.println(a + " >= " + b);
+        }
+        else System.out.println(a + " < " + b);
+    }
+}

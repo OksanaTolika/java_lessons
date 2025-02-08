fa8fdb05190ca150b6c1a78cf8ef3b5a1cacf632
Oksana Tolika <tolika_oksana@mail.ru>
2025-02-08 13:49:40 +0300

    Fix some notes

--- src/lesson3/home/HomeWork3.java
+++ src/lesson3/home/HomeWork3.java
@@ -9,69 +9,58 @@ public class HomeWork3 {
         System.out.println("<--First exercise-->");
         int[] array1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         System.out.println("Array before reverse");
-        printOneArray(array1);
+        printArrayInLine(array1);
         System.out.println("Array after reverse");
-        reverseOneArray(array1);
-        printOneArray(array1);
-        System.out.println();
+        reverseArray(array1);
+        printArrayInLine(array1);
 
         System.out.println("<--Second exercise-->");
         int[] array2 = new int[100];
-        setOneArray(array2);
-        printOneArrayInLine(array2);
-        System.out.println();
+        fillArray(array2);
+        printArrayInLine(array2);
 
         System.out.println("<--Third exercise-->");
         int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.println("Array before change");
-        printOneArrayInLine(array3);
-        changeOneArray(array3);
+        printArrayInLine(array3);
+        changeArray(array3);
         System.out.println("Array after change");
-        printOneArrayInLine(array3);
-        System.out.println();
+        printArrayInLine(array3);
 
         System.out.println("<--Forth exercise-->");
         int[][] array4 = new int[5][5];
-        setTwoArray1(array4);
-        printTwoArray(array4);
-        System.out.println();
+        setArray(array4);
+        printArray(array4);
 
         System.out.println("<--Fifth exercise-->");
-        printOneArrayInLine(setOneArrayInitialValue(8, 6));
-        System.out.println();
+        printArrayInLine(setArrayInitialValue(8, 6));
 
         System.out.println("<--Sixth exercise-->");
         int[] array6 = new int[10];
-        setOneArrayRandomInteger(array6, -20, 20);
-        printOneArrayInLine(array6);
-        printMinMaxElements(array6);
-        System.out.println();
+        setArrayRandomInteger(array6, -20, 20);
+        printArrayInLine(array6);
+        int[] minMaxArray6 = getMinMaxElements(array6);
+        System.out.println("Minimum = " + minMaxArray6[0]);
+        System.out.println("Maximum = " + minMaxArray6[1]);
 
         System.out.println("<--Seventh exercise-->");
         int[] array7 = {4, 5, 3, 1, 1, 2, 2};
-        printOneArrayInLine(array7);
+        printArrayInLine(array7);
         System.out.println(checkBalance(array7));
         System.out.println();
         int[] array8 = {2, 3, 1, 3, 2, 3};
-        printOneArrayInLine(array8);
+        printArrayInLine(array8);
         System.out.println(checkBalance(array8));
-        System.out.println();
     }
 
-    public static void printOneArray(int[] input) {
-        for (int i = 0; i < input.length; i++) {
-            System.out.println("Index #" + i + " = " + input[i]);
-        }
-    }
-
-    public static void printOneArrayInLine(int[] input) {
+    public static void printArrayInLine(int[] input) {
         for (int i = 0; i < input.length; i++) {
             System.out.print(input[i] + "\t");
         }
         System.out.println();
     }
 
-    public static void printTwoArray(int[][] input) {
+    public static void printArray(int[][] input) {
         for (int i = 0; i < input.length; i++) {
             for (int j = 0; j < input[i].length; j++) {
                 System.out.print(input[i][j] + "\t");
@@ -80,7 +69,7 @@ public class HomeWork3 {
         }
     }
 
-    public static void reverseOneArray(int[] array) {
+    public static void reverseArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 1) {
                 array[i] = 0;
@@ -90,13 +79,13 @@ public class HomeWork3 {
         }
     }
 
-    public static void setOneArray(int[] array) {
+    public static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = i + 1;
         }
     }
 
-    public static void changeOneArray(int[] array) {
+    public static void changeArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] < 6) {
                 array[i] *= 2;
@@ -104,26 +93,14 @@ public class HomeWork3 {
         }
     }
 
-    public static void setTwoArray(int[][] array) { //не использую этот метод
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (i == j || i + j == array.length - 1) {
-                    array[i][j] = 1;
-                } else {
-                    array[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    public static void setTwoArray1(int[][] array) {
+    public static void setArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             array[i][i] = 1;
             array[i][array.length - 1 - i] = 1;
         }
     }
 
-    public static int[] setOneArrayInitialValue(int len, int initialValue) {
+    public static int[] setArrayInitialValue(int len, int initialValue) {
         int[] array = new int[len];
         for (int i = 0; i < array.length; i++) {
             array[i] = initialValue;
@@ -135,25 +112,25 @@ public class HomeWork3 {
         return random.nextInt(maxBound - minBound + 1) + minBound;
     }
 
-    public static void setOneArrayRandomInteger(int[] array, int min, int max) {
+    public static void setArrayRandomInteger(int[] array, int min, int max) {
         for (int i = 0; i < array.length; i++) {
             array[i] = myRandomInteger(min, max);
         }
     }
 
-    public static void printMinMaxElements(int[] array) {
-        int min = array[0];
-        int max = array[0];
+    public static int[] getMinMaxElements(int[] array) {
+        int[] minMaxElements = new int[2];
+        minMaxElements[0] = array[0];
+        minMaxElements[1] = array[0];
         for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+            if (array[i] > minMaxElements[1]) {
+                minMaxElements[1] = array[i];
             }
-            if (array[i] < min) {
-                min = array[i];
+            if (array[i] < minMaxElements[0]) {
+                minMaxElements[0] = array[i];
             }
         }
-        System.out.println("Minimum = " + min);
-        System.out.println("Maximum = " + max);
+        return minMaxElements;
     }
 
     public static boolean checkBalance(int[] array) {

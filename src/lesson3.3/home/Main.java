f859d10e5db23d22343680131478815228fa45ab
Oksana Tolika <tolika_oksana@mail.ru>
2025-03-25 16:20:51 +0300

    Fix homework3.3

--- src/lesson3.3/home/Main.java
+++ src/lesson3.3/home/Main.java
@@ -1,11 +1,39 @@
+import java.util.Random;
+
 public class Main {
 
-    public static final int M = 4;
-    public static final int N = 4;
+    public static Random random = new Random();
 
-    public static void printInteger(int[][] matrix) {
+    public static final int minBound = 48;
+    public static final int maxBound = 58;
+
+    public static void main(String[] args) {
+
+        String[][] matrix = createRandomMatrix(4, 4);
+        print(matrix);
+
+        try {
+            printInteger(parseInteger(matrix));
+        } catch (MyArraySizeException myArraySizeException) {
+            System.out.println(myArraySizeException.getMessage());
+        } catch (MyArrayDataException myArrayDataException) {
+            System.out.println(myArrayDataException.getMessage());
+        }
+    }
+
+    public static String[][] createRandomMatrix(int N, int M) {
+        String[][] matrix = new String[N][M];
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
+                matrix[i][j] = String.valueOf((char) myRandomInteger(minBound, maxBound));
+            }
+        }
+        return matrix;
+    }
+
+    public static void printInteger(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
@@ -13,15 +41,32 @@ public class Main {
         System.out.println();
     }
 
-    public static int[][] parseInteger(Matrix matrix) {
-        if ((matrix.m != 4) || (matrix.n != 4)) {
+    public static void print(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int[][] parseInteger(String[][] matrix) {
+        if (matrix.length != 4) {
             throw new MyArraySizeException("Массив должен быть 4 на 4");
         }
-        int[][] matrix1 = new int[matrix.m][matrix.n];
-        for (int i = 0; i < matrix.m; i++) {
-            for (int j = 0; j < matrix.n; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i].length != 4) {
+                throw new MyArraySizeException("Массив должен быть 4 на 4");
+            }
+
+        }
+        int[][] matrix1 = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            matrix1[i] = new int[matrix[i].length];
+            for (int j = 0; j < matrix[i].length; j++) {
                 try {
-                    matrix1[i][j] = Integer.parseInt(matrix.matrix[i][j]);
+                    matrix1[i][j] = Integer.parseInt(matrix[i][j]);
                 } catch (NumberFormatException numberFormatException) {
                     throw new MyArrayDataException("Ошибка в строке " + (i + 1) + ", столбце " + (j + 1));
                 }
@@ -30,17 +75,7 @@ public class Main {
         return matrix1;
     }
 
-    public static void main(String[] args) {
-
-        Matrix matrix = new Matrix(M, N);
-        matrix.print();
-        try {
-            printInteger(parseInteger(matrix));
-
-        } catch (MyArraySizeException myArraySizeException) {
-            System.out.println(myArraySizeException.getMessage());
-        } catch (MyArrayDataException myArrayDataException) {
-            System.out.println(myArrayDataException.getMessage());
-        }
+    public static int myRandomInteger(int minBound, int maxBound) {
+        return random.nextInt(maxBound - minBound + 1) + minBound;
     }
 }
